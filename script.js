f532424dfb1e1297cc11bffb86f15b3a179321be
Masha <m20019999@gmail.com>
2019-03-12 00:31:35 +0300

    1

--- script.js
+++ script.js
@@ -6,21 +6,36 @@
 //   });
 $( ".start" ).click(function() {
   $( ".start" ).fadeOut(3000)
-  $( ".word" ).css( "transform", "rotate(0deg)" );
+  $( ".word" ).css( "transform", "rotate(20deg)" );
   $( ".spin" ).css( {"opacity": "1",
                       "z-index": "101"} );
-  $( ".cards-container" ).css( {"display": "initial",
-                                "opacity": "0" } );
-
 
   setTimeout(function(){
-  $( ".cards-container" ).css( {"opacity": "1",
-                                "z-index": "102"} );
-    $( ".word, .spin" ).css( {"opacity": "0" ,
-                              "transition": "2s"} );
+      $( ".next p" ).css( {"display": "initial" } );
+        $( ".lack p" ).css( {"display": "initial" } );
+    $( ".word, .spin" ).css( {"transition": "5s"} );
   }, 8000);
 
 })
+$( ".next" ).click(function() {
+  $( ".next" ).fadeOut(3000)
+  $( ".word" ).css( "transform", "rotate(1155deg)" );
+  $( ".spin" ).css( {"opacity": "1",
+                      "z-index": "101"} );
+
+  setTimeout(function(){
+      $( ".next p" ).css( {"display": "initial" } );
+    $( ".word, .spin" ).css( {"transition": "1s"} );
+  }, 8000);
+})
+
+$( ".lack" ).click(function() {
+  $( ".lack, .spin" ).fadeOut(3000)
+  $( ".next" ).fadeOut(3000)
+  $( ".content" ).css( {"display": "flex"});
+  $( ".content" ).css( {"transition": "1s"})
+ })
+
 
 
 $( ".hovering-card" ).hover(
@@ -37,33 +52,18 @@ $( ".hovering-card" ).hover(
   }
 );
 
-// $( ".hovering-card" ).click(
-//   function() {
-//     let $cards = $( ".hovering-card" );
-//     $cards.removeClass("hovering-card")
-//     let count = $cards.length;
-//     let step = 15;
-//     for (let i = 0; i < count; i++) {
-//       let angle = -20 + Math.floor( step*i );
-//       $cards.eq(i).css( "transform", "translateX("+angle+"vw)" );
-//     }
-//   }
-// );
-
-
-// let numberOfItems = $( ".date-js" ).length;
-// let index = 0;
-// let y_scale = 0;
-// let x_scale = 0;
-//
-// setInterval(function() {
-//   index = Math.floor( 20*Math.random() );
-//   y_scale =  Math.floor( 1000*Math.random() );
-//   x_scale =  Math.floor( 1000*Math.random() );
-//   $( ".date-js" ).eq( index ).css( "top", y_scale + "px" );
-//   $( ".date-js" ).eq( index ).css( "left", x_scale + "px" );
-// }, 200);
-
+$( ".hovering-card" ).click(
+  function() {
+    let $cards = $( ".hovering-card" );
+    $cards.removeClass("hovering-card")
+    let count = $cards.length;
+    let step = 15;
+    for (let i = 0; i < count; i++) {
+      let angle = -20 + Math.floor( step*i );
+      $cards.eq(i).css( "transform", "translateX("+angle+"vw)" );
+    }
+  }
+);
 
 generator = function (start_x, start_y, direction = 1, x_step = 3, y_step = 150){
   cards_collector = $( ".cards-collector:first" );
@@ -71,7 +71,7 @@ generator = function (start_x, start_y, direction = 1, x_step = 3, y_step = 150)
   let a = 1;
   let b = 10;
   let c = start_y;
-  let x = Math.random()*100 + start_x - 50;
+  let x = Math.random()*50 + start_x - 25;
   let offset = Math.sqrt(Math.abs((start_y*b-c)/a))+start_x;
   let delay = 0;
   let height = $("body").height();
@@ -83,7 +83,7 @@ generator = function (start_x, start_y, direction = 1, x_step = 3, y_step = 150)
     y = y/b;
     let new_card = $("<div class='card'></div>");
     console.log(height+new_card.height())
-    if (y<(height+800)){
+    if (y<(height+400)){
       new_card.css({"top": y+"px",
                    "left": x+"px",
                   "display": "none"});
@@ -115,3 +115,15 @@ setTimeout(function()
       generator(600, 0);
 
   }, 8000);
+// let numberOfItems = $( ".date-js" ).length;
+// let index = 0;
+// let y_scale = 0;
+// let x_scale = 0;
+//
+// setInterval(function() {
+//   index = Math.floor( 20*Math.random() );
+//   y_scale =  Math.floor( 1000*Math.random() );
+//   x_scale =  Math.floor( 1000*Math.random() );
+//   $( ".date-js" ).eq( index ).css( "top", y_scale + "px" );
+//   $( ".date-js" ).eq( index ).css( "left", x_scale + "px" );
+// }, 200);
